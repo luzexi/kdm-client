ee73b4e22be2e2485a4b84b5abd6e21e3dd645cd
luzexi <jesse_luzexi@163.com>
2019-02-01 21:58:30 +0800

    write code

---
+++ u3d/Assets/scripts/util/AssetHelper.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+
+
+public class AssetHelper
+{
+	public static Texture LoadMissionPic(string pic_name)
+	{
+		return null;
+	}
+
+	public static Texture LoadTableMissionPic(string pic_name)
+	{
+		return null;
+	}
+
+	public static bool SaveMissionPic(Texture tex)
+	{
+		//
+		return false;
+	}
+}
